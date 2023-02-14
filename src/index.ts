4542c7f0a559b7c6dfd15c7e491b3c35e9cca867
Anh Nguyen <=>
2023-02-14 11:39:15 +0700

    add pub and config pub

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 
 
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import path from 'path'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
+//pub config
+app.set('view engine', 'pug')
+app.set('views', `${__dirname}/views`)
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server 123456');
+  res.send('Express + TypeScript Server');
 });
+
+app.get('/home', (req: Request, res: Response, next: NextFunction) => {
+  res.render('index' ,{ title: 'FotoBook' })
+})
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
